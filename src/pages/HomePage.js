5a0555eb424d2dad10f02b5ce0502354eb01482d
Ivan <ivanezeh12@gmail.com>
2023-08-09 10:32:09 +0100

    changes to the home page component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,16 +6,7 @@ function Home() {
     <div className="bigBody">
       <div className="HomeContainer">
         <section className="heroSection">
-          <div className="lefty">
-            <h1 className="Heroo">
-              FROZEN IN <br></br>TIME
-            </h1>
-            <span>Capture your finest moments forever.</span>
-            <button className="elsa">Learn More</button>
-          </div>
-          <div className="right">
-            <img className="bigFace" src={bigface} alt="ivanhead" />
-          </div>
+
         </section>
       </div>
     </div>

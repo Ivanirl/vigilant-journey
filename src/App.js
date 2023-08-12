f81f0092df090961d931b381909fd74e026e32f1
Ivan <ivanezeh12@gmail.com>
2023-08-12 13:45:30 +0100

    oh

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
     <Routes>
       <Route path="/" element={<Home/>}></Route>
-      <Route path="portfolio" element={<Portfolio />}></Route>
+      <Route path="/portfolio" element={<Portfolio />}></Route>
     </Routes>
   </DefaultLayout>;
 }

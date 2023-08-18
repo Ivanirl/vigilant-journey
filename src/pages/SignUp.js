c9a7fba50920411b58654dcfd3348672c93ac153
Ivan <ivanezeh12@gmail.com>
2023-08-18 15:50:55 +0100

    new new

---
+++ src/pages/SignUp.js
@@ -0,0 +1,45 @@
+function Signup() {
+  return (
+    <div className="SignUp">
+      <div className="body">
+        <section className="left">
+          <img />
+        </section>
+        <section className="right">
+          <div className="rootbeer">
+            <div className="floating">
+              <img />
+            </div>
+            <div className="input-container">
+              <div className="descript">
+                <div className="header">
+                  <h1>Hey There!</h1>
+                  <span>
+                    Sign in to your account to access your cart and shop!
+                  </span>
+                </div>
+                <button>Sign in with Google</button>
+              </div>
+              <div className="seperator">
+                <div className="rectan lefff"></div>
+                <span>or</span>
+                <div className="rectan righhh"></div>
+              </div>
+              <div className="input-field">
+                <form>
+                <input type="text"></input>
+                <input type="password"></input>
+                <span>Forgot Password</span>
+                <button>Submit</button>
+                <span>Don't have an account? <span>SIgn up</span></span>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;

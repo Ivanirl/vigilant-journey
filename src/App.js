66cc632d0cab0d55c9baa5d0b20aed7fa7dcc240
Ivan <ivanezeh12@gmail.com>
2023-08-18 15:50:20 +0100

    working on sign up page

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import {
 import Home from "./pages/HomePage";
 import Portfolio from "./pages/Portfolio";
 import DefaultLayout from "./components/Layout";
+import Signup from "./pages/SignUp";
 
 function App() {
-  return <DefaultLayout>
-
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      <Route path="/portfolio" element={<Portfolio />}></Route>
-    </Routes>
-    
-  </DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <Routes>
+        <Route path="/Signup" element={<Signup />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/portfolio" element={<Portfolio />}></Route>
+      </Routes>
+    </DefaultLayout>
+  );
 }
 
 export default App;

6c59344698bd0c76fa2ac52c87d4f67d7d3c329f
Ivan <ivanezeh12@gmail.com>
2023-08-21 10:57:21 +0100

    another one

--- src/components/Subbar.js
+++ src/components/Subbar.js
@@ -3,7 +3,6 @@ function Subbar(){
         <div className="Barrister">
             <div className="shoebox"></div>
             <ul></ul>
-            Thus ius a divy div
             <div className="searchbox"></div>
             <div className="favbox"></div>
         </div>

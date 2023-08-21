2bac4ce49ec350679f1801a4599bcf0dd784c6ea
Ivan <ivanezeh12@gmail.com>
2023-08-21 10:59:42 +0100

    hola

--- src/components/Subbar.js
+++ src/components/Subbar.js
@@ -2,7 +2,14 @@ function Subbar(){
     return(
         <div className="Barrister">
             <div className="shoebox"></div>
-            <ul></ul>
+            <ul classname="minibubble">
+                <li>All Products</li>
+                <li>Popular</li>
+                <li>Women</li>
+                <li>Men</li>
+                <li>Kids</li>
+                <li>More</li>
+            </ul>
             <div className="searchbox"></div>
             <div className="favbox"></div>
         </div>

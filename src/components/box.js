d19b938552bc5922b52245fc102c8d74af2b24a1
Ivan <ivanezeh12@gmail.com>
2023-08-23 09:38:02 +0100

    next

---
+++ src/components/box.js
@@ -0,0 +1,31 @@
+export default function Box() {
+  return (
+    <div className="BoxBox">
+      <div className="smaller-box">
+        <div className="top">
+          <img />
+        </div>
+        <div className="bottom">
+          <div className="one">
+            <div className="left">
+              <span className="big">JORDAN</span>
+              <span className="small">Retro Low OG SP</span>
+              <span className="red">Travis Scott Reverse Mocha</span>
+            </div>
+            <div className="right">
+              <span>$1,283</span>
+            </div>
+          </div>
+          <div className="two">
+            <div className="left">
+                <div className="color box"></div>
+            </div>
+            <div className="right">
+                <button>Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

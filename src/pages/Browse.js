cfc93f2ca1879ecd9c0fc449a423f5143363a8d3
Ivan <ivanezeh12@gmail.com>
2023-08-25 14:31:22 +0100

    yeah

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -1,11 +1,15 @@
-import TwoBars from "../components/NavandSubbar"
+import TwoBars from "../components/NavandSubbar";
 import Box from "../components/box";
+import Footer from "../components/Footer";
 
 export default function Browse() {
-  return <div>
-    <TwoBars />
-    <div className="bodice">
+  return (
+    <div>
+      <TwoBars />
+      <div className="bodice">
         <Box />
       </div>
-  </div>;
+      <Footer />
+    </div>
+  );
 }

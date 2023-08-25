d65289a2bd154de5109dcb5115e253efe333bfda
Ivan <ivanezeh12@gmail.com>
2023-08-25 14:30:08 +0100

    yeah

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="container">
+        <div className="left">
+          <img alt="Johnny logo"/>
+          <span>We've got shoes for everyone!</span>
+          <div>
+            <img alt="IG"/>
+            <img alt="FB"/>
+            <img alt="TW"/>
+          </div>
+        </div>
+        <div className="right">
+          <ul className="vought1"></ul>
+          <ul className="vought2"></ul>
+          <ul className="vought3"></ul>
+        </div>
+      </div>
+    </div>
+  );
+}

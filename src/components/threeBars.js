bec3003983db1f29c389e42a1a4ca6f43bcbc67b
Ivan <ivanezeh12@gmail.com>
2023-08-30 11:51:50 +0100

    two

---
+++ src/components/threeBars.js
@@ -0,0 +1,13 @@
+import NavBar from "./navbar";
+import Subbar from "./Subbar";
+import Toggler from "./Toggler";
+
+export default function ThreeBars() {
+  return (
+    <div>
+      <NavBar />
+      <Subbar />
+      <Toggler />
+    </div>
+  );
+}

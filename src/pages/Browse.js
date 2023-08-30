80403cb17e5c3f4d4dc2d99c0f2db12ffa28d141
Ivan <ivanezeh12@gmail.com>
2023-08-30 13:30:32 +0100

    rhf

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -2,13 +2,19 @@ import TwoBars from "../components/NavandSubbar";
 import Box from "../components/box";
 import Footer from "../components/Footer";
 import ThreeBars from "../components/threeBars";
+import BigPic from "../components/BigPic";
 
 export default function Browse() {
   return (
     <div>
       <ThreeBars />
-      <div className="bodice">
-        {/* <Box /> */}
+      <div className="bodice Browse">
+        <BigPic />
+        <div className="caro">
+          <Box />
+          <Box />
+          <Box /> 
+        </div>
       </div>
       {/* <Footer /> */}
     </div>

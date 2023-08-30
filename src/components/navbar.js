8862a5141814dbc5e4aba97bc1279ced7334a89a
Ivan <ivanezeh12@gmail.com>
2023-08-30 13:29:17 +0100

    ee

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,21 +4,8 @@ import Logo from "../Resources/Logogogogo.png";
 
 function NavBar() {
   const [activeLink, setActiveLink] = useState("home");
-  const [scrolled, setScrolled] = useState(false);
   const [menuha, setMenuha] = useState(false);
 
-  useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-    window.addEventListener("scroll", onScroll);
-
-    return () => window.removeEventListener("scroll", onScroll);
-  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
@@ -31,7 +18,7 @@ function NavBar() {
   };
 
   return (
-    <div className="NavBar" id={scrolled ? "scrollo" : ""}>
+    <div className="NavBar">
       <div className="container">
         <div className="rightboy">
           <div className="logo">

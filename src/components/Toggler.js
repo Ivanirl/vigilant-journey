1a07d743b1a196116bc4616471b972427a05aebc
Ivan <ivanezeh12@gmail.com>
2023-09-01 13:58:41 +0100

    six

--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -2,10 +2,13 @@ export default function Toggler() {
   return (
     <div className="toggler">
       <ul className="options">
-        <li className="navbar-link">
+      <li className="navbar-link">
           <span>okay</span>
           <div className="rectangle"></div>
-        </li>
+        </li>        <li className="navbar-link">
+          <span>okay</span>
+          <div className="rectangle"></div>
+        </li> 
       </ul>
       <ul className="end-options">
         <li className="navbar-link">more</li>

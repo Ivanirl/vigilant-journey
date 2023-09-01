9636f3519388cbe2903c47e57bda6b44b87b4207
Ivan <ivanezeh12@gmail.com>
2023-09-01 13:20:36 +0100

    five

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -14,6 +14,10 @@ export default function Browse() {
           <Box />
           <Box />
           <Box /> 
+          <Box /> 
+          <Box /> 
+          <Box /> 
+          <Box /> 
         </div>
       </div>
       {/* <Footer /> */}

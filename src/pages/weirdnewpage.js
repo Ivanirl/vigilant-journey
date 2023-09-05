3be55dcb0b26eefdb45e49e087416ba7550c87a6
Ivan <ivanezeh12@gmail.com>
2023-09-05 22:53:53 +0100

    c

--- src/pages/weirdnewpage.js
+++ src/pages/weirdnewpage.js
@@ -1,5 +1,6 @@
 import NavBar from "../components/navbar";
 import Footer from "../components/Footer";
+import Price from "../components/Pricing";
 
 export default function Random() {
   return (
@@ -7,7 +8,19 @@ export default function Random() {
       <NavBar />
       <div className="bodice">
         <div className="swarm">
-            <h1>BABY</h1>
+          <h1>BABY</h1>
+          <div className="oui">
+            <table>
+              <tr>
+                <td>
+                  <Price />
+                </td>
+                <td>
+                  <Price />
+                </td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
       <Footer />

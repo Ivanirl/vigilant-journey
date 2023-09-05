d8d743b8bd8cd2cbf711543542e42863c8e1a8c9
Ivan <ivanezeh12@gmail.com>
2023-09-05 22:39:30 +0100

    span

---
+++ src/components/Pricing.js
@@ -0,0 +1,13 @@
+export default function Price() {
+  return (
+    <div className="Price">
+      <div className="pricy">
+        <h1>$3,100</h1>
+        <div>
+          <span className="para">Offers Limited features and a few price slashes.</span>
+          <span className="sub">Percentages and whatnot.</span>
+        </div>
+      </div>
+    </div>
+  );
+}

7b3a1f1ee4b5e9d5d9d61002590dec3a1fc7d994
Ivan <ivanezeh12@gmail.com>
2023-09-07 18:12:17 +0100

    she

---
+++ src/pages/oriflame/filler.js
@@ -0,0 +1,12 @@
+import NavBar from "../../components_oriflame/navbar";
+
+export default function Filler() {
+  return (
+    <div>
+      <NavBar />
+      <div className="oho">
+        <h1>this is a filler</h1>
+      </div>
+    </div>
+  );
+}

4e9877d2f79aaabc5cf9553950bd499ea59ac60b
Ivan <ivanezeh12@gmail.com>
2023-09-07 18:13:04 +0100

    braid

---
+++ src/pages/oriflame/filler2.js
@@ -0,0 +1,12 @@
+import NavBar from "../../components_oriflame/navbar";
+
+export default function Filler2() {
+  return (
+    <div>
+      <NavBar />
+      <div className="oho">
+        <h1>this is filler 2</h1>
+      </div>
+    </div>
+  );
+}

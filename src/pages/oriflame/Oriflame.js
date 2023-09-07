f62cfd2064c3670d43685ebd6accc057fe4cf64f
Ivan <ivanezeh12@gmail.com>
2023-09-07 18:13:14 +0100

    bazr

---
+++ src/pages/oriflame/Oriflame.js
@@ -0,0 +1,13 @@
+import NavBar from "../../components_oriflame/navbar";
+
+export default function () {
+  return (
+    <div>
+      <NavBar />
+      <div className="oho">
+        <h1>THIS IS OUR HOMEPAGE</h1>
+        <span>entertwined</span>
+      </div>
+    </div>
+  );
+}

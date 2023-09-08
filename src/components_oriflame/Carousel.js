96203e2ed602e6b8b5ffff38b9662034eef62d53
Ivan <ivanezeh12@gmail.com>
2023-09-08 16:18:16 +0100

    two

---
+++ src/components_oriflame/Carousel.js
@@ -0,0 +1,10 @@
+import herobiggy from "../Resources/Screenshot (141).png";
+
+export default function Carousel(){
+    return(
+        <div className="BigCaro">
+            <img src={herobiggy} />
+            this is a carousel
+        </div>
+    )
+}

c1d07869abef6bdc043ed0d1057890979359d9a3
Ivan <ivanezeh12@gmail.com>
2023-09-08 16:25:32 +0100

    new

--- src/components_oriflame/Carousel.js
+++ src/components_oriflame/Carousel.js
@@ -4,6 +4,7 @@ export default function Carousel(){
     return(
         <div className="BigCaro">
             <img src={herobiggy} />
+            <button>SHOP NOW</button>
             this is a carousel
         </div>
     )

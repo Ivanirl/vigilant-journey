66ea71dfcdd3f34a1ba567adaac82ef9b00b0f66
Ivan <ivanezeh12@gmail.com>
2023-09-10 16:13:52 +0100

    jgj

--- src/pages/oriflame/Oriflame.js
+++ src/pages/oriflame/Oriflame.js
@@ -2,7 +2,7 @@ import NavBar from "../../components_oriflame/navbar";
 import Banner from "../../components_oriflame/Banner";
 import Carousel from "../../components_oriflame/Carousel";
 import LinkBar from "../../components_oriflame/Category_links";
-
+import CardA from "../../components_oriflame/productcardA";
 
 export default function () {
   return (
@@ -10,8 +10,25 @@ export default function () {
       <NavBar />
       <div className="oho">
         <Banner />
-        <Carousel/>
+        <Carousel />
         <LinkBar />
+        <section className="wnsection">
+          <div className="header">
+            <h1>What's new</h1>
+            <button>VIEW ALL</button>
+          </div>
+          <div className="showcase">
+            <div className="imgCont">
+              <img />
+              <button>GET STARTED</button>
+            </div>
+            <div className="products">
+              <CardA />
+              <CardA />
+              <CardA />
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );

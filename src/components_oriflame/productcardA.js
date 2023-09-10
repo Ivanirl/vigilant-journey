d00b34ec95741edbee598c513c3adce9ef10292d
Ivan <ivanezeh12@gmail.com>
2023-09-10 16:13:00 +0100

    no

--- src/components_oriflame/productcardA.js
+++ src/components_oriflame/productcardA.js
@@ -1,10 +1,7 @@
 import Cardinho from "../Resources/IMG_7026.jpg";
-import { useState } from "react";
 import { motion } from "framer-motion";
 
 export default function CardA() {
-  const [overlay, openoverlay] = useState(false);
-
   return (
     <div className="CardA">
       <motion.div
@@ -35,24 +32,8 @@ export default function CardA() {
                 <div className="A circle"></div>
               </div>
             </div>
-            <div className="right">
-              <button
-                onClick={() => {
-                  openoverlay(!overlay);
-                }}
-              >
-                Add to cart
-              </button>
-            </div>
           </div>
         </div>
-      </motion.div>
-      <motion.div
-        initial={{ y: -20, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{ duration: 0.7, ease: [0.6, -0.05, 0.01, 0.99] }}
-        className={overlay ? "visibleO" : "invisibleO"}
-      >
       </motion.div>
     </div>
   );

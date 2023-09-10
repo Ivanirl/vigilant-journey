de83a0cd51fb0e5c81deb7b7105f3d0c23232b61
Ivan <ivanezeh12@gmail.com>
2023-09-10 08:06:38 +0100

    two

--- src/pages/oriflame/Oriflame.js
+++ src/pages/oriflame/Oriflame.js
@@ -1,6 +1,7 @@
 import NavBar from "../../components_oriflame/navbar";
 import Banner from "../../components_oriflame/Banner";
 import Carousel from "../../components_oriflame/Carousel";
+import LinkBar from "../../components_oriflame/Category_links";
 
 
 export default function () {
@@ -10,6 +11,7 @@ export default function () {
       <div className="oho">
         <Banner />
         <Carousel/>
+        <LinkBar />
       </div>
     </div>
   );

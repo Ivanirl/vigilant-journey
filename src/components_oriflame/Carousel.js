b5b15abbc00cfe61ef3f98c086408d3a0098c546
Ivan <ivanezeh12@gmail.com>
2023-09-10 08:07:05 +0100

    three

--- src/components_oriflame/Carousel.js
+++ src/components_oriflame/Carousel.js
@@ -1,11 +1,13 @@
 import herobiggy from "../Resources/Screenshot (141).png";
 
-export default function Carousel(){
-    return(
-        <div className="BigCaro">
-            <img src={herobiggy} />
-            <button className="themebutton1">SHOP NOW</button>
-            this is a carousel
-        </div>
-    )
+export default function Carousel() {
+  return (
+    <div className="BigCaro">
+      <img src={herobiggy} />
+      <div className="rootbeer">
+        {" "}
+        <button className="themebutton1">SHOP NOW</button>
+      </div>
+    </div>
+  );
 }

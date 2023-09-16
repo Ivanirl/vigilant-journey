ece748fc05eab841c358328ab153df70b688e76a
Ivan <ivanezeh12@gmail.com>
2023-09-16 10:13:07 +0100

    strike

---
+++ src/components_oriflame/ArticleThumbnail.js
@@ -0,0 +1,17 @@
+export default function News(props){
+    return(
+        <div className="News">
+            <div className="container">
+            <div className="imgHolder">
+                <img />
+            </div>
+            <div className="txtHolder">
+                <span></span>
+            </div>
+            <div className="buttonHolder">
+                <button>READ MORE</button>
+            </div>
+            </div>
+        </div>
+    )
+}

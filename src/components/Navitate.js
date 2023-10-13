b01997f47f28abaaf46744c386aa93c54867e9eb
Ivan <ivanezeh12@gmail.com>
2023-10-13 11:12:39 +0100

    mmm

---
+++ src/components/Navitate.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function vitate(){
+
+
+    
+    return(
+        <div className="vitate">
+            <ul>
+                <li>one </li>
+                <li> two</li>
+                <li> tree</li> 
+            </ul>
+        </div>
+    )
+}

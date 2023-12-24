007f815a1f68beff2c8bff4f455336c789c69ba9
Ivan <ivanezeh12@gmail.com>
2023-12-24 11:52:51 +0100

    Reasonable stuff

--- src/pages/Preview.js
+++ src/pages/Preview.js
@@ -5,26 +5,8 @@ import Box from "../components/box";
 export default function Preview() {
   return (
     <div>
-      <TwoBars />
       <div className="Preview">
-        <div className="bodice Browse">
-          <BigPic />
-          <section>
-            <div className="left"></div>
-            <div className="rite"></div>
-          </section>
-          <section>
-            <div className="left"></div>
-            <div className="rite"><BigPic /></div>
-          </section>
-          <section>
-            <div>
-              <Box />
-              <Box />
-              <Box />
-            </div>
-          </section>
-        </div>
+        I'm outside babyyyy
       </div>
     </div>
   );
